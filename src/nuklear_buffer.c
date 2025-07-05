4730a406f7a4498e29465ba65e4599d69893ea8a
Luzyvert <raducel.alexandru@gmail.com>
2025-07-05 14:44:54 +0300

    Reapply "Update nuklear_buffer.c"
    This reverts commit 87293877dc8a63090db934ae23d27c135466485c.

--- src/nuklear_buffer.c
+++ src/nuklear_buffer.c
@@ -1,3 +1,13 @@
+#include <string.h>
+#define NK_MEMCPY memcpy
+#define NK_MEMSET memset
+#define NK_SQRT sqrtf
+#define NK_STRICMP strcasecmp
+#define NK_STRNICMP strncasecmp
+#define NK_VSNPRINTF vsnprintf
+#define NK_SINF sinf
+#define NK_COSF cosf
+
 #include "nuklear.h"
 #include "nuklear_internal.h"
 
