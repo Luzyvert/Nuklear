c9a13d277fcd396cfc2a4c070a3dfa4f4a416e51
Luzyvert <raducel.alexandru@gmail.com>
2025-07-05 15:07:54 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -20,25 +20,37 @@ project "Nuklear"
         "../allegro/include"
     }
 
-    libdirs
-    {
-        "../allegro/lib",
-		"../SDL2/lib/x64"
-    }
-
-    links 
-    {
-        "../SDL2/lib/x64/SDL2.lib",
-        "../SDL2/lib/x64/SDL2main.lib",
-        "../SDL2/lib/x64/SDL2test.lib"
-    }
-
     filter "system:windows"
         systemversion "latest"
+		libdirs
+		{
+			"../allegro/lib",
+			"../SDL2/lib/x64"
+		}
+
+		links 
+		{
+			"../SDL2/lib/x64/SDL2.lib",
+			"../SDL2/lib/x64/SDL2main.lib",
+			"../SDL2/lib/x64/SDL2test.lib"
+		}
     
     filter "system:linux"
         pic "On"
         systemversion "latest"
+		libdirs
+		{
+			"../allegro/lib",
+			"../SDL2/linux/lib"
+		}
+
+		links 
+		{
+			"../SDL2/linux/lib/libSDL2.a",
+			"../SDL2/linux/lib/libSDL2.dll.a",
+			"../SDL2/lib/x64/SDL2main.lib",
+			"../SDL2/lib/x64/SDL2test.lib"
+		}
 
     filter "configurations:Debug"
         runtime "Debug"
