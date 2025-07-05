5228e29e33e7f601e8b640dafd8081578f8b2019
Luzyvert <raducel.alexandru@gmail.com>
2025-07-05 10:52:06 +0300

    add premake

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "Nuklear"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.c", "**.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++20"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
