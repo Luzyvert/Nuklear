891f360a40e12ba255ff4148b1c266cf43a70ff4
Luzyvert <raducel.alexandru@gmail.com>
2025-07-05 14:42:24 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,27 @@
 project "Nuklear"
-	kind "StaticLib"
-	language "C++"
+    kind "StaticLib"
+    language "C"
     staticruntime "off"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"src/**.c", "src/**.h", "nuklear.h", "nuklear_sdl_renderer.h"
-	}
+    files
+    {
+        "src/**.c", "src/**.h", "nuklear.h", "nuklear_sdl_renderer.h"
+    }
+
+    defines 
+    {
+        "NK_MEMCPY=memcpy",
+        "NK_MEMSET=memset",
+        "NK_SQRT=sqrtf",
+        "NK_STRICMP=strcasecmp",
+        "NK_STRNICMP=strncasecmp",
+        "NK_VSNPRINTF=vsnprintf",
+        "NK_SINF=sinf",
+        "NK_COSF=cosf"
+    }
 
     includedirs
     {
@@ -22,18 +34,6 @@ project "Nuklear"
 		"../SDL2/lib/x64"
     }
 
-    defines {
-        "NK_MEMCPY=memcpy",
-        "NK_MEMSET=memset",
-        "NK_SQRT=sqrtf",
-        "NK_STRICMP=strcasecmp",
-        "NK_STRNICMP=strncasecmp",
-        "NK_VSNPRINTF=vsnprintf",
-        "NK_SINF=sinf",
-        "NK_COSF=cosf"
-    }
-
-
     links 
     {
         "../SDL2/lib/x64/SDL2.lib",
@@ -41,24 +41,22 @@ project "Nuklear"
         "../SDL2/lib/x64/SDL2test.lib"
     }
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++20"
+    filter "system:windows"
+        systemversion "latest"
+    
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++20"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
 
     filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
+        runtime "Release"
+        optimize "on"
         symbols "off"
